2a98fba4e62fc5a57b553196a93820b76a672764
ValentinoPicc <piccolivalen@gmail.com>
2022-11-24 00:30:00 -0300

    agregando modulos a la consola

---
+++ digitalcomputacion/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <div>
+          <h1>Hola almumnos y alumnas de CoderHouse</h1>
+        </div>
+
+
+        <div>
+          <h1>Hola alumnos y alumnas de CoderHouse</h1>
+          <h2>Como la estan pasando?</h2>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
